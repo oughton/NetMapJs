e7437c01b2cb07125813455d884540be3ca52d32
Joel Oughton <joeloughton@gmail.com>
2011-07-05 16:16:45 +1200

    added mouse events to the overview to drag the view around the main view

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ $NetworkMap.Views = (function() {
 
   return {
     Overview: function(viz, mapOpts) {
-      var opts = jQuery.extend(true, {
+      var _opts = jQuery.extend(true, {
         injectInto: 'overview',
         Node: {
           overridable: true,
@@ -22,39 +22,66 @@ $NetworkMap.Views = (function() {
           type: 'SVG'
         }
       }, mapOpts);
+
+      var _over = new $jit.NetworkMap(_opts);
+      var _container = jQuery('#' + _opts.injectInto);
+      var _mouse = null;
+
+      // translate a main canvas in sync with overview box
+      var _moveBox = function(e) {
+        var o = _container.offset(),
+            x = e.clientX - o.left, y = e.clientY - o.top,
+            canvas = viz.canvas,
+            size = canvas.getSize(),
+            pt1 = _over.c2p({ x: x, y: y }),
+            pt2 = viz.c2p({ x: size.width / 2, y: size.height / 2 });
+
+        canvas.translate(pt2.x - pt1.x, pt2.y - pt1.y);
+      };
       
       // setup overview visualisation
-      var over = new $jit.NetworkMap(opts);
-      var vizSize = viz.canvas.getSize();
-      var overSize = over.canvas.getSize();
-      over.loadJSON(jQuery.extend(true, [], viz.json));
-      
-      over.canvas.resize(overSize.width, 150);
-      over.canvas.scale(overSize.width / vizSize.width, overSize.width / vizSize.width);
-      
-      // add to listen for navigation
-      jQuery(viz.canvas.getElement()).bind('redraw', function() {
-        over.refresh();
-      });
+      var init = function() {
+        var vizSize = viz.canvas.getSize();
+        var overSize = _over.canvas.getSize();
+        
+        _over.loadJSON(jQuery.extend(true, [], viz.json));
+        _over.canvas.resize(overSize.width, 150);
+        _over.canvas.scale(overSize.width / vizSize.width, overSize.width / vizSize.width);
+        
+        // add to listen for navigation
+        jQuery(viz.canvas.getElement()).bind('redraw', function() {
+          _over.refresh();
+        });
 
-      jQuery(over.canvas.getElement()).bind('redraw', function() {
-        var ctx = over.canvas.getCtx(),
-            size = viz.canvas.getSize(), 
-            p1 = viz.c2p({ x: 0, y: 0 }),
-            p2 = viz.c2p({ x: size.width, y: size.height });
+        jQuery(_over.canvas.getElement()).bind('redraw', function() {
+          var ctx = _over.canvas.getCtx(),
+              size = viz.canvas.getSize(), 
+              p1 = viz.c2p({ x: 0, y: 0 }),
+              p2 = viz.c2p({ x: size.width, y: size.height });
 
-        ctx.save();
-        ctx.strokeStyle = 'rgb(255,255,0)';
-        ctx.lineWidth = 10;
-        ctx.strokeRect(p1.x, p1.y, Math.abs(p2.x - p1.x), Math.abs(p2.y - p1.y));
-      });
+          ctx.save();
+          ctx.strokeStyle = 'rgb(255,255,0)';
+          ctx.lineWidth = 10;
+          ctx.strokeRect(p1.x, p1.y, Math.abs(p2.x - p1.x), Math.abs(p2.y - p1.y));
+        });
+
+        // setup mouse events
+        _container.mousedown(function(e) {
+          _moveBox(e);
+          _mouse = e;
+        });
+        _container.mouseup(function() {
+          _mouse = null;
+        });
+        _container.mousemove(function(e) {
+          if (_mouse != null) _moveBox(e);
+        });
+      };
+      
+      init();
 
       return {
-        viz: over,
-
-        translate: function(x, y) {
-          //TODO: implement moving of overview box
-        }
+        viz: _over
       };
     },
   };
