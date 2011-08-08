d86910aac12a58e87968fa31f376925dd5bd49e3
Joel Oughton <joeloughton@gmail.com>
2011-08-09 10:41:59 +1200

    Add tests for overview manager

--- tests/UtilitiesTest.js
+++ tests/UtilitiesTest.js
@@ -47,5 +47,50 @@ jQuery(document).ready(function() {
 
     ok(true, 'created overview');
   });
+  
+  test("Overview Manager Test", function() {
+    _debug.append('<div id="utilitiestest2" style="width:640px;height:100px;background-color:#000"></div>');
+
+    var opts = {
+      injectInto: 'utilitiestest2',
+      layout: 'ForceDirected',
+      Navigation: {
+        enable: true,
+        panning: true,
+        zooming: 40
+      },
+      Node: {
+        overridable: true,
+        dim: 20,
+        lineWidth: 5
+      },
+      Edge: {
+        overridable: true,
+        color: '#23A4FF',
+        lineWidth: 3,
+        type: 'line'
+      },
+      Label: {
+        type: 'SVG'
+      }
+    };
+
+    var main = new $jit.NetworkMap(opts);
+
+    main.loadJSON([
+      {
+        id: 'id1',
+        adjacencies: [
+          { nodeTo: 'id2' }, { nodeTo: 'id3' }
+        ]
+      }
+    ]);
+    main.refresh();
+
+    var container = jQuery('<div id="utilitiestest3" style="width:640px;background-color:#000"></div>').appendTo(_debug);
+    var manager = new $NetworkMap.Views.OverviewManager(main, container, 640, 150);
+
+    ok(true, 'created overview');
+  });
 
 });
