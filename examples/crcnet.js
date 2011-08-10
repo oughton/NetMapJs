e38b13a34f654d9871bab737e46f22d3d3e8446e
Joel Oughton <joeloughton@gmail.com>
2011-08-10 17:14:31 +1200

    Started making the crcnet example better.

--- examples/crcnet.js
+++ examples/crcnet.js
@@ -26,12 +26,13 @@ function init(){
       jQuery.each(obj, function(key, val) {
         switch(key) {
           case 'alias': 
-            host['name'] = val;
+            //host['name'] = val;
             break;
           case 'address':
             break;
           case 'host_name':
-            host['id'] = val.split('-');
+            host['id'] = val;
+            host['name'] = val;
             break;
           case 'parents':
             host['adjacencies'] = [{ nodeTo: val }];
@@ -134,9 +135,9 @@ function init(){
         }
       },
       //Number of iterations for the FD algorithm
-      iterations: 100,
+      iterations: 5000,
       layout: 'ForceDirected',
-      levelDistance: 130,
+      levelDistance: 20,
       bgAlpha: 0.25,
       onCreateLabel: function(domElement, node){
         var _preventDefault = function(evt) { evt.preventDefault(); };
@@ -156,8 +157,6 @@ function init(){
       }
     });
     
-    $NetworkMap.Json.setStartPositions(json, [{"id":"TSWF DSLAM 03","x":"-250","y":"-112"},{"id":"TSWF DSLAM 04","x":"-349","y":"-16"},{"id":"StageETH11","x":"-147","y":"-25"},{"id":"TSWF DSLAM 07","x":"-247","y":"66"},{"id":"Vrfnet","x":"-347","y":"-196"},{"id":"Active Probe #1","x":"-351","y":"165"},{"id":"StagETH10","x":"32","y":"-26"},{"id":"StagETH12","x":"33","y":"-239.9999999"},{"id":"StagePE01-IP01","x":"144.0000001","y":"-97"},{"id":"StagRAN03-21","x":"109","y":"35"},{"id":"Active Probe #2","x":"-100","y":"-240"},{"id":"Cisco Switch #2","x":"144","y":"-212"},{"id":"stagP01-IP04","x":"297","y":"-98"},{"id":"stagP02-IP04","x":"312","y":"38"},{"id":"stagPE20","x":"468","y":"37"},{"id":"stagPE21","x":"470","y":"-97"},{"id":"Active Probe #3","x":"145","y":"-302"},{"id":"Cisco Switch #3","x":"410","y":"-203"},{"id":"Active Probe #4","x":"408","y":"-299"},{"id":"Cisco Switch #4","x":"343","y":"176.0000001"},{"id":"IPERF Server","x":"446","y":"176"},{"id":"Active Probe #5","x":"344","y":"271"},{"id":"Brix Server","x":"239","y":"177"},{"id":"DSL Modem #1","x":"-344","y":"-297"}]);
-
     $NetworkMap.Utils.Metrics.initJSON(json);
 
     // load JSON data.
@@ -168,34 +167,22 @@ function init(){
     var debug = new $NetworkMap.Debug.GraphicalOutput(fd);
     debug.enable();
 
+    fd.canvas.scale(0.25, 0.25);
+
     fd.computeIncremental({
       iter: 40,
       property: 'end',
       onStep: function(perc){
         debug.logWrite(perc + '% loaded...');
+        fd.end();
       },
       onComplete: function(){
         debug.logWrite('done');
-        fd.animate({
-          modes: ['linear'],
-          transition: $jit.Trans.Elastic.easeOut,
-          duration: 2500
-        });
+        fd.end();
       }
     });
 
-    //fd.refresh();
-    
     // overview test
-    var over = new $NetworkMap.Views.Overview(fd, { injectInto: 'overview' });
-    
-    var btnSave = jQuery('<button>Save</button>').click(function() {
-      var positions = [];
-      fd.graph.eachNode(function(node) {
-          positions.push({ id: node.id, x: node.pos.x, y: node.pos.y });
-      });
-      jQuery.post('../src/networkmap.php?method=savePositions', { positions: positions });
-    }).appendTo(document.body);
-    // end
+    var over = new $NetworkMap.Views.OverviewManager(fd, jQuery('#overview'), 180, 150);
   }
 }
