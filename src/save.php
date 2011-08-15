15ac55408dc00e4bb80ace8c03c5781ff972e296
Joel Oughton <joeloughton@gmail.com>
2011-08-15 20:55:15 +1200

    Added a function to the save script that makes for pretty printing of json.

--- src/save.php
+++ src/save.php
@@ -3,7 +3,87 @@ $filename = $_GET['filename'];
 if (stripos($filename, "/") != False ) {die("bad filename $filename");}
 if (stripos($filename, "./") != False ) {die("bad filename $filename");}
 $fh = fopen("../public/" . $filename, 'w') or die("can't open file");
-fwrite($fh, json_encode($_POST['json'], JSON_NUMERIC_CHECK));
+fwrite($fh, json_format(json_encode($_POST['json'], JSON_NUMERIC_CHECK)));
 fclose($fh);
+ 
+// Pretty print some JSON 
+function json_format($json) 
+{ 
+    $tab = "  "; 
+    $new_json = ""; 
+    $indent_level = 0; 
+    $in_string = false; 
+
+    $json_obj = json_decode($json); 
+
+    if($json_obj === false) 
+        return false; 
+
+    $json = json_encode($json_obj); 
+    $len = strlen($json); 
+
+    for($c = 0; $c < $len; $c++) 
+    { 
+        $char = $json[$c]; 
+        switch($char) 
+        { 
+            case '{': 
+            case '[': 
+                if(!$in_string) 
+                { 
+                    $new_json .= $char . "\n" . str_repeat($tab, $indent_level+1); 
+                    $indent_level++; 
+                } 
+                else 
+                { 
+                    $new_json .= $char; 
+                } 
+                break; 
+            case '}': 
+            case ']': 
+                if(!$in_string) 
+                { 
+                    $indent_level--; 
+                    $new_json .= "\n" . str_repeat($tab, $indent_level) . $char; 
+                } 
+                else 
+                { 
+                    $new_json .= $char; 
+                } 
+                break; 
+            case ',': 
+                if(!$in_string) 
+                { 
+                    $new_json .= ",\n" . str_repeat($tab, $indent_level); 
+                } 
+                else 
+                { 
+                    $new_json .= $char; 
+                } 
+                break; 
+            case ':': 
+                if(!$in_string) 
+                { 
+                    $new_json .= ": "; 
+                } 
+                else 
+                { 
+                    $new_json .= $char; 
+                } 
+                break; 
+            case '"': 
+                if($c > 0 && $json[$c-1] != '\\') 
+                { 
+                    $in_string = !$in_string; 
+                } 
+            default: 
+                $new_json .= $char; 
+                break;                    
+        } 
+    } 
+
+    return $new_json; 
+} 
 ?>
+
 
