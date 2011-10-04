0970e5259e8ab7544a8a1d6b6c844c7b87c83264
Joel Oughton <joeloughton@gmail.com>
2011-10-05 11:44:12 +1300

    Added a public API php script. Need to replace all php calls with calls to this API.

---
+++ src/networkmap.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Public API for client to server side interaction.
+ *
+ * TODO: replace client side PHP calls with only calls to this API.
+ *
+ */
+
+function saveState() {
+  // TODO: Implement
+}
+
+function loadState() {
+  // TODO: Implement
+}
+
+function pollData() {
+  // TODO: Implement
+}
+
+?>
